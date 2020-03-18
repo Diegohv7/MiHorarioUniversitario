c2c3ffccf022dab9f5ffd11739a1e6e067f1b41c
jsoliz064 <62267283+jsoliz064@users.noreply.github.com>
2020-03-18 11:32:10 -0400

    splash

--- app/src/main/java/com/example/mihorariouniversitario/SplashActivity.java
+++ app/src/main/java/com/example/mihorariouniversitario/SplashActivity.java
@@ -2,13 +2,14 @@ package com.example.mihorariouniversitario;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity  {
 
     private final int DURACION_SPLASH = 2000;
 
