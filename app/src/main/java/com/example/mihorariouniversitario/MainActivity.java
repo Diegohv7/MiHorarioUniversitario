5e5d09c81896ec1f615c9ebd2689b90ebb202be0
jsoliz064 <62267283+jsoliz064@users.noreply.github.com>
2020-03-19 01:48:04 -0400

    tema añadido

--- app/src/main/java/com/example/mihorariouniversitario/MainActivity.java
+++ app/src/main/java/com/example/mihorariouniversitario/MainActivity.java
@@ -1,16 +1,46 @@
 package com.example.mihorariouniversitario;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.CheckedTextView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Switch;
+
+import java.util.zip.CheckedInputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ConstraintLayout fondo;
+    private Switch  tema;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fondo=(ConstraintLayout)  findViewById(R.id.fondo);
+        tema= findViewById(R.id.switch1 );
+
+    }
+
+    public void eventoClick(View view){
+
+        if (tema.isChecked() ){
+            fondo.setBackgroundColor(Color.BLACK);
+        }else {
+            fondo.setBackgroundColor(Color.WHITE );
+        }
+
+
     }
 
 
@@ -18,4 +48,7 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.overflow, menu);
         return true;
     }
-}
+
+    }
+
+
