d9156961d4a06f478217998f9fc7dda95a42dd3d
Diegohv7 <62267951+Diegohv7@users.noreply.github.com>
2020-03-27 21:31:57 -0400

    commit

--- app/src/main/java/com/example/mihorariouniversitario/AgendaActivity.java
+++ app/src/main/java/com/example/mihorariouniversitario/AgendaActivity.java
@@ -1,14 +1,75 @@
 package com.example.mihorariouniversitario;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.CalendarView;
 
-public class AgendaActivity extends AppCompatActivity {
+public class AgendaActivity extends AppCompatActivity implements CalendarView.OnDateChangeListener {
+
+
+    private CalendarView calendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agenda);
+
+
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener(this);
+    }
+
+    @Override
+    public void onSelectedDayChange(CalendarView calendarView, int i, int i1, int i2) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        CharSequence []items = new CharSequence[3];
+        items[0] = "Agregar eventos";
+        items[1] = "Ver eventos";
+        items[2] = "Cancelar";
+
+        final int dia, mes, año;
+        int mes1;
+        dia = i;
+        mes1 = i1;
+        año = i2;
+        mes1++;
+
+        mes = mes1;
+        builder.setTitle("Seleccione una tarea")
+                .setItems(items, new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialoginterface, int i) {
+                        if (i == 0) {
+                            Intent intent = new Intent(getApplication(), AddeventosActivity.class);
+                            Bundle bundle = new Bundle();
+                            bundle.putInt("día", dia);
+                            bundle.putInt("mes", mes);
+                            bundle.putInt("año", año);
+                            intent.putExtras(bundle);
+                            startActivity(intent);
+
+                        } else if (i == 1) {
+                            Intent intent = new Intent(getApplication(), ListeventosActivity.class);
+                            Bundle bundle = new Bundle();
+                            bundle.putInt("día", dia);
+                            bundle.putInt("mes", mes);
+                            bundle.putInt("año", año);
+                            intent.putExtras(bundle);
+                            startActivity(intent);
+
+                        } else {
+                            return;
+                        }
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
